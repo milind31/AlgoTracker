a51a343e5fecf2be810252462d95c86aba664db2
milind31 <milind.rv@gmail.com>
2021-03-22 22:59:40 -0700

    create signup model

--- AlgorithmicTrading/main/views.py
+++ AlgorithmicTrading/main/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .forms import EmailSignupForm
+from .models import Signup
+
+def email_list_signup(request):
+    form = EmailSignupForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            email_signup_qs = Signup.objects.filter(email=form.instance.email)
+            if email_signup_qs.exists():
+                messages.info(request, "EMAIL ALREADY EXISTS")
+            else:
+                form.save()
+        
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 def homepage(request):
-    return HttpResponse("<h1>Hello</h1<")
+    form = EmailSignupForm()
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'main/home.html', context)
